dd9ee0e4e2eafae15bff635d6ae88223f558c7ef
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-11-24 14:33:22 +0100

    texte centre

--- src/gui/menu/menu.cpp
+++ src/gui/menu/menu.cpp
@@ -9,20 +9,27 @@ Menu::Menu(float width, float height){
         exit(1);
     }
 
+    
     menu[0].setFont(font);
     menu[0].setFillColor(sf::Color::Red);
     menu[0].setString("Play");
-    menu[0].setPosition(sf::Vector2f(width/2, height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
+    sf::FloatRect textRect = menu[0].getLocalBounds();
+    menu[0].setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
+    menu[0].setPosition(sf::Vector2f(width/2.0f, height/(MAX_NUMBER_OF_ITEMS + 1) * 1.0f));
 
     menu[1].setFont(font);
     menu[1].setFillColor(sf::Color::White);
     menu[1].setString("Create");
-    menu[1].setPosition(sf::Vector2f(width/2, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
+    textRect = menu[1].getLocalBounds();
+    menu[1].setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
+    menu[1].setPosition(sf::Vector2f(width/2.0f, height / (MAX_NUMBER_OF_ITEMS + 1) * 2.0f));
 
     menu[2].setFont(font);
     menu[2].setFillColor(sf::Color::White);
     menu[2].setString("Test");
-    menu[2].setPosition(sf::Vector2f(width/2, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
+    textRect = menu[2].getLocalBounds();
+    menu[2].setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
+    menu[2].setPosition(sf::Vector2f(width/2.0f, height / (MAX_NUMBER_OF_ITEMS + 1) * 3.0f));
 
     //add more menus here and change MAX_NUMBER_OF_ITEMS in "menu.hpp"
 
