48c5ca7a53803a2cb582921560f7a0f8d83134f6
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-11-26 16:45:11 +0100

    updated menu conditionnals

--- src/gui/menu/menu.cpp
+++ src/gui/menu/menu.cpp
@@ -14,22 +14,22 @@ Menu::Menu(float width, float height){
     menu[0].setFillColor(sf::Color::Red);
     menu[0].setString("Play");
     sf::FloatRect textRect = menu[0].getLocalBounds();
-    menu[0].setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
-    menu[0].setPosition(sf::Vector2f(width/2.0f, height/(MAX_NUMBER_OF_ITEMS + 1) * 1.0f));
+    menu[0].setOrigin(textRect.left + textRect.width/2, textRect.top + textRect.height/2);
+    menu[0].setPosition(sf::Vector2f(width/2, height/(MAX_NUMBER_OF_ITEMS + 1) * 1));
 
     menu[1].setFont(font);
     menu[1].setFillColor(sf::Color::White);
     menu[1].setString("Create");
     textRect = menu[1].getLocalBounds();
-    menu[1].setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
-    menu[1].setPosition(sf::Vector2f(width/2.0f, height / (MAX_NUMBER_OF_ITEMS + 1) * 2.0f));
+    menu[1].setOrigin(textRect.left + textRect.width/2, textRect.top + textRect.height/2);
+    menu[1].setPosition(sf::Vector2f(width/2, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
 
     menu[2].setFont(font);
     menu[2].setFillColor(sf::Color::White);
     menu[2].setString("Test");
     textRect = menu[2].getLocalBounds();
-    menu[2].setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
-    menu[2].setPosition(sf::Vector2f(width/2.0f, height / (MAX_NUMBER_OF_ITEMS + 1) * 3.0f));
+    menu[2].setOrigin(textRect.left + textRect.width/2, textRect.top + textRect.height/2);
+    menu[2].setPosition(sf::Vector2f(width/2, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
 
     //add more menus here and change MAX_NUMBER_OF_ITEMS in "menu.hpp"
 
@@ -82,15 +82,21 @@ void Menu::event_handler(sf::Event event){
 			switch (GetPressedItem()){
 			    case 0:
 		            std::cout << "Play button has been pressed" << std::endl;
+                    //action :
+
                     this->is_on = 0;
 					break;
 				case 1:
 					std::cout << "Create button has been pressed" << std::endl;
+                    //action :
+
                     this->is_on = 0;
 					break;
 				case 2:
 					std::cout << "Test button has been pressed" << std::endl;
-					this->is_on = 0;
+					//action :
+                    
+                    this->is_on = 0;
                     break;
 			}
 	}
