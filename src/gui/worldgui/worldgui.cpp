fc775f01708f429f2f007f290e22901bec1ae1a8
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-12-26 16:29:32 +0100

    added comments

--- src/gui/worldgui/worldgui.cpp
+++ src/gui/worldgui/worldgui.cpp
@@ -5,6 +5,7 @@
 Worldgui::Worldgui(int input_width, int input_height, Terrain T){
     this->height = input_height;
     this->width = input_width;
+    //off by default
     this->is_on = 0;
 
     sizeoftile_x = width/T.getX();
@@ -12,12 +13,20 @@ Worldgui::Worldgui(int input_width, int input_height, Terrain T){
 }
 
 void Worldgui::draw(sf::RenderWindow &window, Terrain T){
-    sf::RectangleShape tiles[T.getX()][T.getY()];
-    sf::RectangleShape overlay[T.getX()][T.getY()];
+    
+    //generate random bool
+    auto gen = std::bind(std::uniform_int_distribution<>(0,1), std::default_random_engine());
+
+    //create all needed shapes grids
+    sf::RectangleShape grass[T.getX()][T.getY()];
+    sf::RectangleShape objects[T.getX()][T.getY()];
+    
+    //create all needed textures
     sf::Texture grass1;
     sf::Texture grass2;
     sf::Texture tree_large;
-    auto gen = std::bind(std::uniform_int_distribution<>(0,1), std::default_random_engine());
+
+    //load all needed textures
     if(!grass1.loadFromFile("res/sprites/tileGrass1.png")){
         cerr << "Failed to load grass1 texture";
     }
@@ -27,28 +36,33 @@ void Worldgui::draw(sf::RenderWindow &window, Terrain T){
     if(!tree_large.loadFromFile("res/sprites/treeGreen_large.png")){
         cerr << "failed to load tree_large";
     }
-
+    
+    //drawing loop
     for(int x = 0; x < T.getX(); x++){
         for(int y = 0; y < T.getY(); y++){
-            if(gen()){
-                    tiles[x][y].setSize(sf::Vector2f(sizeoftile_x, sizeoftile_y));
-                    tiles[x][y].setPosition(x*sizeoftile_x, y*sizeoftile_y);
-                    tiles[x][y].setTexture(&grass1);
+            
+            if(gen()){ //display two types of grass
+                    grass[x][y].setSize(sf::Vector2f(sizeoftile_x, sizeoftile_y));
+                    grass[x][y].setPosition(x*sizeoftile_x, y*sizeoftile_y);
+                    grass[x][y].setTexture(&grass1);
                 }
                 else{
-                    tiles[x][y].setSize(sf::Vector2f(sizeoftile_x, sizeoftile_y));
-                    tiles[x][y].setPosition(x*sizeoftile_x, y*sizeoftile_y);
-                    tiles[x][y].setTexture(&grass2);
+                    grass[x][y].setSize(sf::Vector2f(sizeoftile_x, sizeoftile_y));
+                    grass[x][y].setPosition(x*sizeoftile_x, y*sizeoftile_y);
+                    grass[x][y].setTexture(&grass2);
                 }
+
             if(T.getEntite(x,y) != nullptr){
                 if(T.getEntite(x,y)->getType() == "arbre"){
-                    overlay[x][y].setSize(sf::Vector2f(sizeoftile_x, sizeoftile_y));
-                    overlay[x][y].setPosition(x*sizeoftile_x, y*sizeoftile_y);
-                    overlay[x][y].setTexture(&tree_large);
+                    objects[x][y].setSize(sf::Vector2f(sizeoftile_x, sizeoftile_y));
+                    objects[x][y].setPosition(x*sizeoftile_x, y*sizeoftile_y);
+                    objects[x][y].setTexture(&tree_large);
                 }
             }
-            window.draw(tiles[x][y]);
-            window.draw(overlay[x][y]);
+            //draw grass layer
+            window.draw(grass[x][y]);
+            //draw object on top of grass
+            window.draw(objects[x][y]);
         }
     }
 }
